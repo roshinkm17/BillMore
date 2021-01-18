6aa245e1793f52f00f35fec67f70edc67b06b8e8
sharangravi <ssskunhimangalam@lbscek@ac.in>
2021-01-18 22:17:37 +0530

    kali

--- lib/screens/registration_screen_two.dart
+++ lib/screens/registration_screen_two.dart
@@ -1,4 +1,7 @@
+
 import 'package:biller/components/CustomInputField.dart';
+import 'package:biller/components/mainButton.dart';
+import 'package:biller/screens/registration_screen_three.dart';
 import 'package:flutter/material.dart';
 import 'package:biller/utility/company.dart';
 
@@ -18,28 +21,81 @@ class _RegistrationScreenTwoState extends State<RegistrationScreenTwo> {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
                 "Need some more details",
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
-              SizedBox(height: 20),
+              //SizedBox(height: 20),
+              CustomInputField(
+                placeholder: "Name of the firm",
+                onChanged: (value) {
+                  setState(() {
+                    company.name = value;
+                  });
+                },
+              ),
+              //SizedBox(height: 20),
+              CustomInputField(
+                placeholder: "GST Number",
+                onChanged: (value) {
+                  setState(() {
+                    company.gstNumber = value;
+                  });
+                },
+              ),
+              //SizedBox(height: 20),
+              CustomInputField(
+                placeholder: "Address",
+                onChanged: (value) {
+                  setState(() {
+                    company.address = value;
+                  });
+                },
+              ),
+              //SizedBox(height: 20),
+              CustomInputField(
+                placeholder: "Phone Number",
+                onChanged: (value) {
+                  setState(() {
+                    company.phone = value;
+                  });
+                },
+              ),
+              //SizedBox(height: 20),
+              CustomInputField(
+                placeholder: "Email ID",
+                onChanged: (value) {
+                  setState(() {
+                    company.email = value;
+                  });
+                },
+              ),
+              //SizedBox(height: 20),
+              CustomInputField(
+                placeholder: "Mobile Number",
+                onChanged: (value) {
+                  setState(() {
+                    company.mobile = value;
+                  });
+                },
+              ),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  CustomInputField(
-                    placeholder: "Name of the firm",
-                    onChanged: (value) {
-                      setState(() {
-                        company.name = value;
-                      });
+                  MainButton(
+                    buttonText: "Continue",
+                    onPressed: (){
+                      Navigator.pushNamed(context, RegistrationScreenThree.id);
                     },
                   ),
                 ],
               ),
             ],
           ),
+
         ),
       ),
     );
