2789ffa71ea5988b850ec58af22f0a7d154d9474
nivinanil <sheena123anil>
2021-01-20 20:12:52 +0530

    pdf

--- lib/screens/pdf_preview_screen.dart
+++ lib/screens/pdf_preview_screen.dart
@@ -7,10 +7,8 @@ class PdfPreviewScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      child: PdfPreviewScreen(
-        path: path,
-      ),
+    return PdfPreviewScreen(
+      path: path,
     );
   }
 }
